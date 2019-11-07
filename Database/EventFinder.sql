d1d29c5e51240026df9bda8ab78031a2f3f1b8b4
Ashley <carl7268@bears.unco.edu>
2019-11-06 17:06:53 -0700

    Version 2 of the Database

--- Database/EventFinder.sql
+++ Database/EventFinder.sql
@@ -1,56 +1,62 @@
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `user_id` int PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(32) NOT NULL,
   `email` varchar(64) NOT NULL,
+  `hash` varchar(255) NOT NULL,
   `first_name` varchar(32) NOT NULL,
   `last_name` varchar(32) NOT NULL,
   `acct_type` int(1) NOT NULL,
-  `crt_timestmp` datetime NOT NULL,
-  `updt_timestmp` datetime NOT NULL
+  `create_timestmp` datetime NOT NULL,
+  `updt_timestmp` datetime 
 );
 
 CREATE TABLE `event` (
   `event_id` int PRIMARY KEY AUTO_INCREMENT,
-  `validated` tinyint(1) NOT NULL,
   `event_name` varchar(32) NOT NULL,
   `event_desc` varchar(1024),
-  `contacts` int NOT NULL,
-  `has_food` tinyint(1) NOT NULL,
+  `has_food` bit(1) DEFAULT 0,
   `event_time` datetime NOT NULL,
-  `location` varchar(128),
-  `club` int,
-  `url_social` char(128),
-  `crt_timestmp` datetime NOT NULL,
-  `updt_timestmp` datetime NOT NULL
+  `latitude` float,
+  `longitude` float,
+  `location` varchar(512),
+  `external_url` varchar(512),
+  `create_timestmp` datetime NOT NULL,
+  `updt_timestmp` datetime 
 );
 
 CREATE TABLE `club` (
   `club_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(32),
-  `contacts` int,
-  `fac_sponsor` int,
-  `crt_timestmp` datetime NOT NULL,
-  `updt_timestmp` datetime NOT NULL
+  `club_name` varchar(32),
+  `fac_sponsor_id` int,
+  `create_timestmp` datetime NOT NULL,
+  `updt_timestmp` datetime 
 );
 
 CREATE TABLE `userclub` (
-  `user` int,
-  `club` int
+  `user_id` int,
+  `club_id` int,
+  `is_contact` bit(1) DEFAULT 0
 );
 
-CREATE TABLE `eventFollowers` (
-  `event` int,
-  `follower` int
+CREATE TABLE `eventUser` (
+  `event_id` int,
+  `user_id` int,
+  `is_following` bit(1) DEFAULT 0
 );
 
-ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `event` (`contacts`);
+CREATE TABLE `clubEvent` (
+  `event_id` int,
+  `club_id` int
+);
 
-ALTER TABLE `event` ADD FOREIGN KEY (`club`) REFERENCES `club` (`club_id`);
+ALTER TABLE `userclub` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `userclub` ADD FOREIGN KEY (`user`) REFERENCES `users` (`user_id`);
+ALTER TABLE `userclub` ADD FOREIGN KEY (`club_id`) REFERENCES `club` (`club_id`);
 
-ALTER TABLE `club` ADD FOREIGN KEY (`club_id`) REFERENCES `userclub` (`club`);
+ALTER TABLE `clubEvent` ADD FOREIGN KEY (`club_id`) REFERENCES `club` (`club_id`);
 
-ALTER TABLE `eventFollowers` ADD FOREIGN KEY (`follower`) REFERENCES `users` (`user_id`);
+ALTER TABLE `event` ADD FOREIGN KEY (`event_id`) REFERENCES `clubEvent` (`event_id`);
 
-ALTER TABLE `event` ADD FOREIGN KEY (`event_id`) REFERENCES `eventFollowers` (`event`);
+ALTER TABLE `eventUser` ADD FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `eventUser` (`user_id`);
